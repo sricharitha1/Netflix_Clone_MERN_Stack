94a4f2cd1ee603964188ea1053218631687838eb
sricharitha1 <charithas040@gmail.com>
2025-10-22 02:52:14 +0530

    fix: update CORS config for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173",
+      "https://netflix-clone-mern-stack-lyart.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
