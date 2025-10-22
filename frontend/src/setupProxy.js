c19310a50b6baef82b55a6cc03758ebdf667abff
sricharitha1 <charithas040@gmail.com>
2025-10-22 14:21:50 +0530

    Remove setupProxy.js and use VITE_API_BASE_URL for API calls

--- frontend/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export default function(app) {
-  app.use('/api', createProxyMiddleware({
-    target: 'http://localhost:9000',
-    changeOrigin: true,
-  }));
-}
